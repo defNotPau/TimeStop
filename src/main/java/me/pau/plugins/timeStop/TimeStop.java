4babd840cbfddd924a46e53c6387808f335cd906
defNotPau <pablo.cabarcas@gmail.com>
2024-11-11 14:20:24 -0500

    Initial commit

---
+++ src/main/java/me/pau/plugins/timeStop/TimeStop.java
@@ -0,0 +1,17 @@
+package me.pau.plugins.timeStop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TimeStop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
